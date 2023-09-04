b7e9c0e1672ad1a3530dee64a4468dbb80cad355
liizahh <142186798+liizahh@users.noreply.github.com>
2023-09-04 17:09:24 +0800

    Add files via upload

---
+++ Lizah_PBO.css
@@ -0,0 +1,46 @@
+body {
+    font-family: '', Georgia, 'Times New Roman', Times, serif;
+    margin: 20px;
+    background-color: #cbb39b;
+}
+
+header {
+    background-color: #3333;
+    color: #000000;
+    text-align: center;
+    padding: 20px;
+}
+
+.profil img {
+    max-width: 20%;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+    margin-left: 30px;
+}
+
+h1 {
+    font-size: 36px;
+}
+
+h2 {
+    font-size: 24px;
+    color: #555;
+}
+
+p {
+    font-size: 16px;
+    line-height: 1.5;
+}
+
+.content {
+    background-color: #fff;
+    padding: 20px;
+    margin: 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+img {
+    max-width: 100%;
+    height: auto;
+}
